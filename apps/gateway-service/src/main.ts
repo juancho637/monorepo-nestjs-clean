4660fd9e4bd695e521dcb378a88f671ffaf7cebe
Juan David Garcia <juancho637@gmail.com>
2025-03-29 08:53:58 -0500

    add chages

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     isGlobal: true,
   });
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
